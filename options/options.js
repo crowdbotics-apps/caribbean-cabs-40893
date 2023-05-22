c95d97f4445904e0850cab1d79fec5173c4ce356
Crowdbotics <team@crowdbotics.com>
2023-05-22 18:31:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "caribbean-cabs-40893",
+  url: "https://caribbean-cabs-40893.botics.co",
+  api: "https://caribbean-cabs-40893.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
